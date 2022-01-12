4cae194d301ee8d4ee3003ba88608d42e5ff66dc
H4x0R-KS <63690995+H4x0R-KS@users.noreply.github.com>
2022-01-12 16:31:03 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,9 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("Repo", "https://github.com/H4x0R-KS/ks-mirror")
+    buttons.buildbutton("Group", "https://t.me/KS380")
+    buttons.buildbutton("Owner of Repo & Bot", "https://t.me/King_Sahan")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -154,9 +155,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='Slam Mirrorbot Help',
-        author_name='Slam Mirrorbot',
-        author_url='https://github.com/SlamDevs/slam-mirrorbot',
+        title='KS Mirrorbot Help',
+        author_name='KingSahan Mirrorbot',
+        author_url='https://github.com/H4x0R-KS/ks-mirror',
         html_content=help_string_telegraph,
     )["path"]
 
