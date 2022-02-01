6beedecf079e591ccb53b0482299333b75657946
H4x0R-KS <63690995+H4x0R-KS@users.noreply.github.com>
2022-02-01 09:25:56 +0530

    Update aria.bat

--- aria.bat
+++ aria.bat
@@ -1,1 +1,1 @@
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=12 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=15 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true
